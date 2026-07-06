24f1f1f21a26821f95a3b337188daad6eec39d54
SophearithSaing <sophearithsaing123@gmail.com>
2026-07-06 23:14:52 +0700

    Use app router

--- lib/app.dart
+++ lib/app.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'core/routing/app_router.dart';
+
 class LAdditionApp extends StatelessWidget {
   const LAdditionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "L'Addition",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1C1C1A)),
         useMaterial3: true,
       ),
-      home: const _AppPlaceholder(),
+      routerConfig: appRouter,
     );
-  }
-}
-
-class _AppPlaceholder extends StatelessWidget {
-  const _AppPlaceholder();
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("L'Addition")));
   }
 }
